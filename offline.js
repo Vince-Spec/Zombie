7edffd10c68087ef93d6c73c8f789c7fe244da16
Vince-Spec <122793549+Vince-Spec@users.noreply.github.com>
2024-05-22 08:11:10 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1716336351,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player_spr-sheet0.png",
+		"images/enemy_spr-sheet0.png",
+		"images/bullet_spr-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
